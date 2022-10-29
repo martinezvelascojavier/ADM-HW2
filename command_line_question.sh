fc69b68d492ddd87d0c31edeaa7d22ac708c9742
martinezvelascojavier <113454044+martinezvelascojavier@users.noreply.github.com>
2022-10-29 15:20:04 +0200

    Update command_line_question.sh

--- command_line_question.sh
+++ command_line_question.sh
@@ -5,7 +5,14 @@ unzip instagram_posts.zip
 wget https://adm2022.s3.amazonaws.com/instagram_profiles.zip
 unzip instagram_profiles.zip
 
-cat instagram_posts.csv | awk 'length($8) > 100 && !seen[$4]++' | head -10 > top10profiles.csv
+#First we create a new file containing the first ten rows in the instagram_posts.csv in which the description column contains more that 100 characters 'length($8) > 100'
+#But we ommit those lines in which the comments are the same '!seen[$8]++'
+
+cat instagram_posts.csv | awk 'length($8) > 100 && !seen[$8]++' | head -10 > top10profiles.csv
+
+#Finally we print their ids, and if their sid_profile is -1 ($2 != -1) it means they are not in instagram_profiles.csv, so we output "user not found"
+#However, within this ids, all the users are in instagram_profiles.csv. 
+#After testing our code, we learned that the first id that would output a "User not found" would appear if we print the top 80 users, instead of top 10.
 
 awk '{
 if($2 != -1)
@@ -18,4 +25,4 @@ print "User not found", "\n"
 }
  
 }' top10profiles.csv
-~                    
+                   
